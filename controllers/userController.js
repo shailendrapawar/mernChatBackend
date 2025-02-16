a8bd17496b222d511534d5c91975db821350979b
shailendr pawar <shailendrapawar980@gmail.com>
2025-02-16 23:07:35 +0530

    ADDED: login API functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
 import UserModel from "../models/userModel.js"
 import bcrypt from "bcrypt"
-
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
 class UserController {
 
     static register = async (req, res) => {
         try {
+
             const { username, email, password, gender } = req.body
+
+            if (!username || !gender || !password || !email) {
+                return res.status(401).json({
+                    msg: "enter all credentials"
+                })
+            }
 
             const isUser = await UserModel.findOne({ $or: [{ email }, { username }] })
             if (isUser) {
@@ -35,31 +44,74 @@ class UserController {
 
             const isCreated = await newUser.save();
 
-            console.log(isCreated)
-
             if (isCreated) {
                 return res.status(201).json({
                     msg: "account created"
                 })
             }
-
-
         } catch (err) {
-
             console.log(err)
-           return res.status(400).json({
-                msg:"account not created",
-            
+            return res.status(400).json({
+                msg: "account not created",
             })
-
         }
         // res.send("resgister working")
 
     }
 
     static login = async (req, res) => {
-        console.log(req.body)
-        res.send("login working")
+
+        try {
+
+            const { identifier, password } = req.body;
+
+            const isUser = await UserModel.findOne({ $or: [{ email: identifier }, { username: identifier }] });
+            if (!isUser) {
+                return res.status(401).json({
+                    msg: "User does'nt exist"
+                })
+
+            } else {
+                // console.log(isUser)
+                const isCorrect = await bcrypt.compareSync(password, isUser.password)
+
+                if (!isCorrect) {
+                    return res.status(400).json({
+                        msg: "invalid credentials"
+                    })
+                }
+
+                const tokenSecretKey = process.env.TOKEN_SECRET
+                const tokenData = {
+                    userId: isUser._id
+                }
+                const token = await jwt.sign(tokenData, tokenSecretKey, { expiresIn: "3d" })
+
+                return res.status(200).json({
+                    msg: "login successfully",
+                    token: token,
+                    user: {
+                        id:isUser._id,
+                        username:isUser.username,
+                        profile:isUser.profileImg,
+                        email:isUser.email
+                    }
+                })
+            }
+        } catch (err) {
+            console.log(err)
+
+            return res.status(401).json({     
+                msg: "Something went wrong"
+            })
+
+        }
+
+
+
+
+
+
     }
 
 
