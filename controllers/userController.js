adaa0c20d547aa223464fda9d0ae09b6fa7fe0cf
shailendr pawar <shailendrapawar980@gmail.com>
2025-02-22 16:04:17 +0530

    ADDED: minor changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -88,7 +88,7 @@ class UserController {
                 }
                 const token = await jwt.sign(tokenData, tokenSecretKey, { expiresIn: "3d" })
 
-                return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true, sameSite: "strict" }).json({
+                return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 *1000, httpOnly: true, sameSite: "strict" }).json({
                     msg: "login successfully",
                     token: token,
                     user: {
