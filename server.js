db9c050fc38b696bbfb2f10f6c7529b7c25c2879
shailendr pawar <shailendrapawar980@gmail.com>
2025-02-24 23:56:03 +0530

    ADDED: minor changes

--- server.js
+++ server.js
@@ -10,23 +10,28 @@ import messageRouter from "./routes/messageRoute.js"
 // ===============================================
 
 // ============== socket imports================
-import {app,myServer,io} from "./socket/socket.js"
+import { app, myServer, io } from "./socket/socket.js"
 
 //==============middelwares imports==============
 import cors from "cors"
 import cookieParser from "cookie-parser";
 app.use(cors({
-    origin:["https://mern-chat-frontend-sepia.vercel.app/","*","http://localhost:5173"],
-    credentials:true,
-    methods:["POST","GET"],
+    origin: ["https://mern-chat-frontend-sepia.vercel.app/", "*", "http://localhost:5173"],
+    credentials: true,
+    methods: ["POST", "GET"],
 }))
-app.use(express.urlencoded({extended:true}));
+app.options("*", cors({
+    origin: ["https://mern-chat-frontend-sepia.vercel.app/", "*", "http://localhost:5173"],
+    credentials: true,
+    methods: ["POST", "GET"],
+}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(express.json())
-app.use("/auth",userRouter)
-app.use("/user",messageRouter)
+app.use("/auth", userRouter)
+app.use("/user", messageRouter)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("server working")
 })
 
@@ -34,8 +39,8 @@ app.get("/",(req,res)=>{
 
 
 
-const PORT=process.env.PORT||5000
-myServer.listen(PORT,()=>{
+const PORT = process.env.PORT || 5000
+myServer.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
 
 })
