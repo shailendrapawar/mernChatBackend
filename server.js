4339512ef7648cd31995c0821c3509b00c21c6dc
shailendr pawar <shailendrapawar980@gmail.com>
2025-02-25 12:35:01 +0530

    ADDED: cors error resolving

--- server.js
+++ server.js
@@ -15,18 +15,22 @@ import { app, myServer, io } from "./socket/socket.js"
 //==============middelwares imports==============
 import cors from "cors"
 import cookieParser from "cookie-parser";
+
 app.use(cors({
     origin: ["https://mern-chat-frontend-sepia.vercel.app/", "*", "http://localhost:5173"],
     credentials: true,
     methods: ["POST", "GET"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
 app.options("*", cors({
     origin: ["https://mern-chat-frontend-sepia.vercel.app/", "*", "http://localhost:5173"],
     credentials: true,
     methods: ["POST", "GET"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(express.json())
